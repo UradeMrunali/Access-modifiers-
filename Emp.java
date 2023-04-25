d45cebf94a233ccc80101458e11333116f7cfbf8
Mrunali <mrunaliurade26@gmail.com>
2023-04-25 19:26:51 +0530

    example of access modifiers

---
+++ Emp.java
@@ -0,0 +1,14 @@
+package PackB;
+
+public class Emp {
+  int id;
+  public String name;
+  private double salary;
+  protected int ph_no;
+  
+  void show()
+  {
+	  System.out.println(id+" "+name+" "+salary+" "+ph_no);
+  }
+  
+}
